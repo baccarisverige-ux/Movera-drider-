906c75c7aef94b56d10e4c7aeb367ece9272bdcc
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-21 01:54:24 +0200

    Add app composition root for driver dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:movera/constants/appcolors.dart';
+import 'package:movera/core/location/driver_location_repository.dart';
+import 'package:movera/core/location/driver_location_service.dart';
+import 'package:movera/core/routing/road_route_service.dart';
+import 'package:movera/core/routing/route_repository.dart';
+import 'package:movera/core/session/driver_session_controller.dart';
+import 'package:movera/core/waybill/waybill.dart';
 import 'package:movera/presentation/driver/home/home.dart';
 
 void main() {
   runApp(const MoveraApp());
 }
 
-class MoveraApp extends StatelessWidget {
+/// App composition root.
+///
+/// Long-lived frontend dependencies are created here and injected down into
+/// feature screens. When backend adapters replace the current frontend
+/// implementations, screen code does not need to change.
+class MoveraApp extends StatefulWidget {
   const MoveraApp({super.key});
+
+  @override
+  State<MoveraApp> createState() => _MoveraAppState();
+}
+
+class _MoveraAppState extends State<MoveraApp> {
+  late final DriverSessionController _session;
+  late final WaybillRepository _waybills;
+  late final DriverLocationRepository _location;
+  late final RouteRepository _routing;
+
+  @override
+  void initState() {
+    super.initState();
+    _session = DriverSessionController();
+    _waybills = InMemoryWaybillRepository.instance;
+    _location = const DriverLocationService();
+    _routing = RoadRouteService();
+  }
+
+  @override
+  void dispose() {
+    _session.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,12 +56,17 @@ class MoveraApp extends StatelessWidget {
       ensureScreenSize: true,
       builder: (_, child) {
         return GetMaterialApp(
-          title: 'MoveraApp App',
+          title: 'Movera Driver',
           debugShowCheckedModeBanner: false,
           theme: ThemeData.light(
             useMaterial3: true,
           ).copyWith(scaffoldBackgroundColor: AppColor.bg),
-          home: const DriverHome(),
+          home: DriverHome(
+            sessionController: _session,
+            waybillRepository: _waybills,
+            locationRepository: _location,
+            routeRepository: _routing,
+          ),
         );
       },
     );
